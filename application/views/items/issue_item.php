e565b8df35e0ebc6eb4e8f5806f60937ad006811
arparthasarathi <arparthasarathi@gmail.com>
2015-08-11 11:20:10 +0530

    Minor UI Changes

--- application/views/items/issue_item.php
+++ application/views/items/issue_item.php
@@ -1,95 +1,129 @@
-<style type="text/css">
-.controls {
-margin: 75px;
-}
+<script>
+   var selectedItems = null;
+   selectedItems = <?php if(isset($selectedItems)) echo json_encode($selectedItems); else echo json_encode(NULL);?>;
+   var selectedQuantity = <?php if(isset($selectedQuantity)) echo json_encode($selectedQuantity); 
+                                else echo json_encode(NULL);?>;
+   $(function(){
+         var tabindex = 1;
+         $('input,select,button,date').each(function() {
+               if (this.type != "hidden") {
+                     var $input = $(this);
+                     $input.attr("tabindex", tabindex);
+                     tabindex++;
+               }
+         });
+         $('#issuedDate').pickadate();
 
-select {
-border-size: 2px;
-border-color: #000066;
-border-radius: 4px;
-}
+         if(selectedItems)
+         {
+               var selectedMess = <?php if(isset($selectedMess)) echo json_encode($selectedMess);
+                                        else echo json_encode(NULL);?>;
+               var issuedDate = <?php if(isset($issuedDate)) echo json_encode($issuedDate);
+                                      else echo json_encode(NULL);?>;
+                console.log(selectedMess);
+                $("select#selectedMess").find('option[value="' + selectedMess + '"]').prop('selected', true);
+                $("#issuedDate").pickadate('picker').set('select',new Date(issuedDate));
+               for(var i=0;i<selectedItems.length;i++)
+               {
+                     var quantityID = "#txt"+selectedItems[i];
+                     var quantity = selectedQuantity[i];
+                     $(quantityID).attr('value',quantity);
+                     $(quantityID).css('background-color','#ffff00');
+               }
+         }
+   });
+   function change_color(obj)
+   {
+         if($(obj).val().length !== 0) $(obj).css('background-color','#ffff00');
+         else $(obj).css('background-color','#fff');
+   }
 
-.controls a {
-	border-radius: 4px;
-	font-size: 15px;
-	text-align: center;
-	width: 100px;
-}
-
-.btn-large{
-	height:60px;
-	font-size: 20px;
-	width: 150px;
-}
-
-</style>
+</script>
 <?php
-$decodedTableData = json_decode($tableData,true);
-$size = count($decodedTableData['itemNames']);
+   $decodedTableData = json_decode($tableData,true);
+   $size = count($decodedTableData['itemNames']);
 ?>
-	<form name="selection" method="post"  action="issue_item"> 
-	<div class="row">
+<form name="selection" method="post"  action="issue_item" onsubmit="return get_list();"> 
+   <div class="row">
 
-		<div class="input-field col s6 offset-s3">
-			<select class="browser-default" name="selectedMess" value = "<?php echo isset($selectedMess) ? $selectedMess : "";?>" required>
-				<option></option>
-				<?php
-					foreach($messTypes as $eachType)	
-					{
-					?>
-					<option value='<?php echo $eachType;?>'><?php echo $eachType;?></option>
-					<?php
-					}	
-				?>
-			</select>
-		</div>
-	</div>
+      <div class="input-field col s6">
+         <select class="browser-default" name="selectedMess" value = "<?php echo isset($selectedMess) ? $selectedMess : "";?>" required tabindex=1 id="selectedMess" autofocus>
+            <option value="">Select Mess</option>
+            <?php
+               foreach($messTypes as $eachType)	
+               {
+               ?>
+               <option value='<?php echo $eachType;?>' id='<?php echo $eachType;?>'><?php echo $eachType;?></option>
+               <?php
+               }	
+            ?>
+         </select>
+      </div>
+      <div class = "input-field col s6">
+         <input type="date" class="datepicker" id="issuedDate" name="issuedDate" placeholder='Select Date' required/>
+      </div>
 
-	<div class="row">
-			<?php
-				for($i=0;$i<count($decodedTableData['itemNames']);$i++)
-				{
-				?>
-		<div class='row'>
-		<div class = "input-field col s4">
-		<p>
-			<input type='checkbox' value='<?php echo $decodedTableData['itemNames'][$i];?>' 
-				id='<?php echo $decodedTableData['itemNames'][$i];?>' 
-					onclick="document.getElementById('<?php echo 'txt'.$decodedTableData['itemNames'][$i];?>').disabled=!this.checked;"
-					 name='selectedItems[]'/>
-					<label for='<?php echo $decodedTableData['itemNames'][$i];?>'><?php echo $decodedTableData['itemNames'][$i];?></label>
-		</p>
-		</div>
-		<div class="input-field col s4">
-                        <label class="blue-text text-darken-2"><?php echo $decodedTableData['quantityAvailable'][$i];?></label>
-                        <input type="hidden" name='quantityAvailable[]' value='<?php echo $decodedTableData['quantityAvailable'][$i];?>'/>
-                        <input type="hidden" name='latestRate[]' value='<?php echo $decodedTableData['latestRate'][$i];?>'/>
-                </div>
-                <div class="input-field col s4">
-                        <input type="text" name="selectedQuantity[]" value="" id='<?php echo 'txt'.$decodedTableData['itemNames'][$i];?>' disabled/>
-                        <label for="last_name">Enter Quantity</label>
-                </div>
-		
-		</div>
-				<?php
-				}
-			?>
+   </div>
+   <div class='row'>
+      <div class='col s4'>
+         <span class='blue-text text-darken-2'>Item Name</span>
+      </div>
 
-	<div class="row">
-		<div class="col s8 offset-s3">
+      <div class='col s4'>
+         <span class='blue-text text-darken-2'>Quantity Available(Kg/L)</span>
+      </div>
 
-			 <button class="btn waves-effect waves-light btn-large" 
-					value="submit" type="submit" name="submit">
-			 Submit
-			    <i class="glyphicon glyphicon-chevron-right"></i>
-			 </button>
+      <div class='col s4'>
+         <span class='blue-text text-darken-2'>Quantity Required(Kg/L)</span>
+      </div>
 
-			 <button class="btn waves-effect waves-light red darken-1 btn-large" 
-					value="cancel" type="cancel" name="cancel">
-			 Cancel
-			    <i class="glyphicon glyphicon-remove"></i>
-			</button>
-		</div>
-	</div>
-	</form> 
+   </div>
+
+
+   <?php
+      for($i=0;$i<count($decodedTableData['itemNames']);$i++)
+      {
+      ?>
+      <div class="row margin_row">
+         <div class = "col s4 ">
+            <p>
+            <input type='text' value='<?php echo $decodedTableData['itemNames'][$i];?>' 
+            id='<?php echo $decodedTableData['itemNames'][$i];?>' 
+            name='selectedItems[]' readonly/>
+            <!--					<label for='<?php /* echo $decodedTableData['itemNames'][$i];?>'><?php /*echo $decodedTableData['itemNames'][$i];?></label>*/?> -->
+            </p>
+         </div>
+         <div class="col s4">
+            <input type="text" name="selectedQuantity[]" value="" id='<?php echo 'txt'.$decodedTableData['itemNames'][$i];?>' placeholder='Enter Quantity' onchange="javascript:change_color(this)"/>
+            <!--                        <label for="last_name">Enter Quantity</label>-->
+         </div>
+         <div class="col s4">
+            <span class="blue-text text-darken-2"><h5><?php echo $decodedTableData['quantityAvailable'][$i];?></h5></span>
+            <input type="hidden" name='quantityAvailable[]' value='<?php echo $decodedTableData['quantityAvailable'][$i];?>'/>
+            <input type="hidden" name='latestRate[]' value='<?php echo $decodedTableData['latestRate'][$i];?>'/>
+         </div>
+
+      </div>
+      <?php
+      }
+   ?>
+
+
+   <div class="row">
+      <div class="col s8 offset-s3">
+
+         <button class="btn waves-effect waves-light btn-large" 
+            value="submit" type="submit" name="submit">
+            Submit
+            <i class="glyphicon glyphicon-chevron-right"></i>
+         </button>
+
+         <button class="btn waves-effect waves-light red darken-1 btn-large" 
+            value="cancel" type="cancel" name="cancel">
+            Cancel
+            <i class="glyphicon glyphicon-remove"></i>
+         </button>
+      </div>
+   </div>
+</form> 
 </div>
--- application/views/items/return_item.php
+++ application/views/items/return_item.php
@@ -1,182 +1,196 @@
 <script>
-$(function(){
-    $('#selectedMess').change(function(){
-	
-         var messName = encodeURIComponent($(this).val());
-	 var providedItems = $("#issuedItems");
-	$(providedItems).html("");
-         $.ajax({
-            url : 'getMessConsumptionForToday/'+messName,
-            type : 'GET'  ,
-	    dataType : 'json',
-            success : function(data){
-		console.log(data);
-		$(data.itemNames).each(function(index){
+   function generate_elements(data,tabindex,providedItems)
+   {
+         console.log(data);
+         $('.info').hide();
+         if((data.itemNames.length) == 0) $('.info').show();
+         $(data.itemNames).each(function(index){
 
-		var divRow = $(document.createElement('div')).attr({
-					class: 'row'
-				});
+               var divRow = $(document.createElement('div')).attr({
+                     class: 'row margin_row'
+               });
 
-		var divSelected = $(document.createElement('div')).attr({
-					class: 'input-field col s4'
-					});
+               var divSelected = $(document.createElement('div')).attr({
+                     class: 'col s4'
+               });
 
-		$(divRow).append(divSelected);
+               $(divRow).append(divSelected);
 
-		$(divSelected).append(
-			$(document.createElement('input')).attr({
-         			  id:    data.itemNames[index]
-			          ,name:  'selectedItems[]'
-			          ,value: data.itemNames[index]
-			          ,type:  'checkbox'
-      				 })
-			);
-
-		$(divSelected).append(
-			$(document.createElement('label')).attr({
-         			  for:    data.itemNames[index]
-			          }).html(data.itemNames[index])
-			);
+               $(divSelected).append(
+                  $(document.createElement('input')).attr({
+                        id:    data.itemNames[index]
+                        ,name:  'selectedItems[]'
+                        ,value: data.itemNames[index]
+                        ,type:  'text'
+                        ,tabindex: tabindex++
+                        ,readonly: 'readonly' 
+                  })
+               );
 
 
-		
-		var divSelected1 = $(document.createElement('div')).attr({
-					class: 'input-field col s4'
-					});
-	
-		
+               var divSelected2 = $(document.createElement('div')).attr({
+                     class: 'col s4'
+               });
 
 
 
+               $(divRow).append(divSelected2);
 
-		$(divRow).append(divSelected1);
-
-                $(divSelected1).append(
-                        $(document.createElement('label')).attr({
-				class: 'blue-text text-darken-2'
-                                 }).html(data.quantitySupplied[index])
-                        );
-
-		 $(divSelected1).append(
-                        $(document.createElement('input')).attr({
-                                  id:    data.quantitySupplied[index]
-                                  ,name:  'quantitySupplied[]'
-                                  ,value: data.quantitySupplied[index]
-                                  ,type:  'hidden'
-                                 })
-                        );
-		
-		 $(divSelected1).append(
-                        $(document.createElement('input')).attr({
-                                  id:    data.latestRate[index]
-                                  ,name:  'latestRate[]'
-                                  ,value: data.latestRate[index]
-                                  ,type:  'hidden'
-                                 })
-                        );
-
-		var divSelected2 = $(document.createElement('div')).attr({
-					class: 'input-field col s4'
-					});
-	
-
-
-		$(divRow).append(divSelected2);
-
-                $(divSelected2).append(
-                        $(document.createElement('input')).attr({
-				  id : 'txt'+data.itemNames[index]
-                                  ,name:  'selectedQuantity[]'
-                                  ,type:  'text'
-                                 })
-                        );
-		$(divSelected2).append(
-                        $(document.createElement('label')).attr({
-                                  for:   'txt'+data.itemNames[index]
-                                  }).html('Enter Quantity')
-                                 
-                        );
+               $(divSelected2).append(
+                  $(document.createElement('input')).attr({
+                        id : 'txt'+data.itemNames[index]
+                        ,name:  'selectedQuantity[]'
+                        ,type:  'text'
+                        ,placeholder: 'Enter Quantity'
+                        ,onchange:"javascript:change_color(this)"
+                        ,tabindex: tabindex++
+                  })
+               );
 
 
 
-		$(providedItems).append(divRow);
 
-		var txtBox = document.getElementById('txt'+data.itemNames[index]);
-		txtBox.disabled = true;
-		var chkBox = document.getElementById(data.itemNames[index]);
-		chkBox.onchange = function() {
-			txtBox.disabled=!chkBox.checked;
-			}
+               var divSelected1 = $(document.createElement('div')).attr({
+                     class: 'col s4'
+               });
 
-		});
-            }
-         }); 
 
-    });
-}) 
-</script>
-<style type="text/css">
-.controls {
-margin: 75px;
-}
 
-select {
-border-size: 2px;
-border-color: #000066;
-border-radius: 4px;
-}
 
-.controls a {
-        border-radius: 4px;
-        font-size: 15px;
-        text-align: center;
-        width: 100px;
-}
 
-.btn-large{
-        height:60px;
-        font-size: 20px;
-        width: 150px;
-}
 
-</style>
-	<form name="selection" method="post"  action="return_item"> 
-	<div class="row">
-		<div class="input-field col s6 offset-s3">
-			<select class="browser-default" id="selectedMess" name="selectedMess" value = "<?php echo isset($selectedMess) ? $selectedMess : "";?>" required>
-			<option></option>
-			<?php	
-			foreach($messTypes as $eachType)
-			{
-			?>
-			<option value='<?php echo $eachType;?>'><?php echo $eachType;?></option>
-			<?php
-			}
-			?>
-			</select>
-		</div>
-	</div>
+               $(divRow).append(divSelected1);
 
-	<div class="row">
-			<div id="issuedItems">
-			</div>
-	</div>
+               $(divSelected1).append(
+                  $(document.createElement('label')).attr({
+                        class: 'blue-text text-darken-2'
+                  }).html(data.quantitySupplied[index])
+               );
 
-	<div class="row">
-                <div class="col s8 offset-s3">
+               $(divSelected1).append(
+                  $(document.createElement('input')).attr({
+                        id:    data.quantitySupplied[index]
+                        ,name:  'quantitySupplied[]'
+                        ,value: data.quantitySupplied[index]
+                        ,type:  'hidden'
+                  })
+               );
 
-                         <button class="btn waves-effect waves-light btn-large" 
-                                        value="submit" type="submit" name="submit">
-                         Submit
-                            <i class="glyphicon glyphicon-chevron-right"></i>
-                         </button>
+               $(divSelected1).append(
+                  $(document.createElement('input')).attr({
+                        id:    data.latestRate[index]
+                        ,name:  'latestRate[]'
+                        ,value: data.latestRate[index]
+                        ,type:  'hidden'
+                  })
+               );
 
-                         <button class="btn waves-effect waves-light red darken-1 btn-large" 
-                                        value="cancel" type="cancel" name="cancel">
-                         Cancel
-                            <i class="glyphicon glyphicon-remove"></i>
-                        </button>
-                </div>
-        </div>
-	</form> 
+
+
+
+               $(providedItems).append(divRow);
+
+
+         });
+      }
+      $(function(){
+            $('#issuedDate').pickadate();
+            var tabindex = 1;
+            $('select,input').each(function() {
+                  if (this.type != "hidden") {
+                        var $input = $(this);
+                        $input.attr("tabindex", tabindex);
+                        tabindex++;
+                  }
+            });
+
+            $('#selectedMess').change(function(){
+
+                  var messName = encodeURIComponent($(this).val());
+                  var issuedDate = encodeURIComponent($('#issuedDate').val());
+                  var providedItems = $("#issuedItems");
+                  $(providedItems).html("");
+                  $.ajax({
+                        url : 'getMessConsumptionForDate/'+messName+'/'+issuedDate,
+                        type : 'GET'  ,
+                        dataType : 'json',
+                        success : function(data){
+                              generate_elements(data,tabindex,providedItems);	
+                        }
+                  }); 
+
+            });
+
+            $('#issuedDate').change(function(){
+
+                  var messName = encodeURIComponent($('#selectedMess').val());
+                  var issuedDate = encodeURIComponent($(this).val());
+                  var providedItems = $("#issuedItems");
+                  $(providedItems).html("");
+                  $.ajax({
+                        url : 'getMessConsumptionForDate/'+messName+'/'+issuedDate,
+                        type : 'GET'  ,
+                        dataType : 'json',
+                        success : function(data){
+                              generate_elements(data,tabindex,providedItems);	
+                        }
+                  }); 
+
+            });
+
+
+      }) 
+   </script>
+   <form name="selection" method="post"  action="return_item"> 
+      <div class="row">
+         <div class="input-field col s6">
+            <select class="browser-default" id="selectedMess" name="selectedMess" value = "<?php echo isset($selectedMess) ? $selectedMess : "";?>" required>
+               <option value="">Select Mess</option>
+               <?php	
+                  foreach($messTypes as $eachType)
+                  {
+                  ?>
+                  <option value='<?php echo $eachType;?>'><?php echo $eachType;?></option>
+                  <?php
+                  }
+               ?>
+            </select>
+         </div>
+         <div class = "input-field col s6">
+            <input type="date" class="datepicker" id="issuedDate" name="issuedDate" placeholder='Select Date' required/>
+         </div>
+
+      </div>
+
+      <div class="row">
+         <div id="issuedItems">
+         </div>
+      </div>
+
+
+      <div class="info row">
+         <div class="col s8 offset-s2">
+            <span class="blue-text text-darken-2"><h5>Select an item from the list for return.</h5></span>
+         </div>
+      </div>
+
+
+
+      <div class="row">
+         <div class="col s8 offset-s3">
+
+            <button class="btn waves-effect waves-light btn-large" 
+               value="submit" type="submit" name="submit">
+               Submit
+               <i class="glyphicon glyphicon-chevron-right"></i>
+            </button>
+
+            <button class="btn waves-effect waves-light red darken-1 btn-large" 
+               value="cancel" type="cancel" name="cancel">
+               Cancel
+               <i class="glyphicon glyphicon-remove"></i>
+            </button>
+         </div>
+      </div>
+   </form> 
 </div>
